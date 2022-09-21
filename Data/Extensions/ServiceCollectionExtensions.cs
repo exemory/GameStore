64d6daf95cd4bf0c55bc67cb28363faab087b587
Vlad <MadVlad1715@gmail.com>
2022-09-21 17:52:12 +0300

    Add repository registration using Scrutor

--- Data/Extensions/ServiceCollectionExtensions.cs
+++ Data/Extensions/ServiceCollectionExtensions.cs
@@ -10,8 +10,16 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddDataLayer(this IServiceCollection services, string connectionString,
         bool enableSensitiveDataLogging)
     {
-        services.AddScoped<IGameRepository, GameRepository>();
-        services.AddScoped<ICommentRepository, CommentRepository>();
+        services.Scan(s =>
+        {
+            s.FromAssembliesOf(typeof(IRepository<>))
+                .AddClasses(classes =>
+                    classes.AssignableTo(typeof(Repository<>))
+                        .Where(t => t != typeof(Repository<>)))
+                .AsMatchingInterface()
+                .WithScopedLifetime();
+        });
+
         services.AddScoped<IUnitOfWork, UnitOfWork>();
 
         services.AddDbContext<GameStoreContext>(options =>
