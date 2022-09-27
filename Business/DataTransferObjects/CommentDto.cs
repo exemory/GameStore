d4f2e3d32785c06abaf5f725bd916fea323370c7
Vlad <MadVlad1715@gmail.com>
2022-09-27 09:37:17 +0300

    Fix unnecessary nullable types to regular

--- Business/DataTransferObjects/CommentDto.cs
+++ Business/DataTransferObjects/CommentDto.cs
@@ -3,8 +3,8 @@
 public class CommentDto
 {
     public Guid Id { get; set; }
-    public string? Name { get; set; }
-    public string? Body { get; set; }
+    public string Name { get; set; } = default!;
+    public string Body { get; set; } = default!;
 
     public Guid? ParentId { get; set; }
 }
