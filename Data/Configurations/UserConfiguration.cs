29c6276d29032dc67b2b1c2bbdf3a2eed65cb914
Vlad <MadVlad1715@gmail.com>
2022-10-13 13:17:58 +0300

    Add user to the db context

---
+++ Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.Property(u => u.FirstName)
+            .HasMaxLength(50);
+        
+        builder.Property(u => u.LastName)
+            .HasMaxLength(50);
+    }
+}
--- Data/GameStoreContext.cs
+++ Data/GameStoreContext.cs
@@ -1,10 +1,12 @@
 ﻿using Data.Configurations;
 using Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Data;
 
-public class GameStoreContext : DbContext
+public class GameStoreContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
 {
     public GameStoreContext(DbContextOptions options) : base(options)
     {
@@ -14,14 +16,15 @@ public class GameStoreContext : DbContext
     public DbSet<Comment> Comments { get; set; } = default!;
     public DbSet<Genre> Genres { get; set; } = default!;
     public DbSet<PlatformType> PlatformTypes { get; set; } = default!;
-    
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
 
-        modelBuilder.ApplyConfiguration(new GameConfiguration());
-        modelBuilder.ApplyConfiguration(new CommentConfiguration());
-        modelBuilder.ApplyConfiguration(new GenreConfiguration());
-        modelBuilder.ApplyConfiguration(new PlatformTypeConfiguration());
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new GameConfiguration());
+        builder.ApplyConfiguration(new CommentConfiguration());
+        builder.ApplyConfiguration(new GenreConfiguration());
+        builder.ApplyConfiguration(new PlatformTypeConfiguration());
+        builder.ApplyConfiguration(new UserConfiguration());
     }
 }
