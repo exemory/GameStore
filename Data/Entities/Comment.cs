93c679693040165c3a400e1bc3488804110aa276
Vlad <MadVlad1715@gmail.com>
2022-10-27 15:15:46 +0300

    Update comment entity, add migration

--- Data/Entities/Comment.cs
+++ Data/Entities/Comment.cs
@@ -3,10 +3,15 @@
 public class Comment : EntityBase
 {
     public string Body { get; set; } = default!;
+    public DateTimeOffset CreationDate { get; set; }
+    public bool Deleted { get; set; }
 
     public Guid? ParentId { get; set; }
     public Comment? Parent { get; set; }
 
     public Guid GameId { get; set; }
     public Game Game { get; set; } = default!;
+    
+    public Guid UserId { get; set; }
+    public User User { get; set; } = default!;
 }
---
+++ Data/Migrations/20221027121030_UpdateComment.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class UpdateComment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Body",
+                table: "Comments",
+                type: "nvarchar(600)",
+                maxLength: 600,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreationDate",
+                table: "Comments",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValueSql: "GETUTCDATE()");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Comments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Comments",
+                type: "uniqueidentifier",
+                nullable: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_UserId",
+                table: "Comments",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "CreationDate",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Body",
+                table: "Comments",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(600)",
+                oldMaxLength: 600);
+        }
+    }
+}
