49bedf6869f00b458cad9ab77e9c172e8c815569
Vlad <MadVlad1715@gmail.com>
2022-10-28 20:50:47 +0300

    Add CommentUpdateDto validator

---
+++ Business/Validators/CommentUpdateDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using Business.DataTransferObjects;
+using FluentValidation;
+
+namespace Business.Validators;
+
+public class CommentUpdateDtoValidator : AbstractValidator<CommentUpdateDto>
+{
+    public CommentUpdateDtoValidator()
+    {
+        RuleFor(c => c.Body)
+            .NotEmpty()
+            .MaximumLength(600);
+    }
+}
