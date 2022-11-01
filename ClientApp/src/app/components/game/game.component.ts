5bc4443a74e390655530f5d6d829cbed76dba83e
Vlad <MadVlad1715@gmail.com>
2022-11-01 20:37:59 +0200

    Add comments to the game

--- ClientApp/src/app/components/game/game.component.ts
+++ ClientApp/src/app/components/game/game.component.ts
@@ -5,6 +5,9 @@ import {GameWithDetails} from "../../interfaces/game-with-details";
 import {NotificationService} from "../../services/notification.service";
 import {Game} from "../../interfaces/game";
 import {environment as env} from "../../../environments/environment";
+import {Comment} from "../../interfaces/comment";
+import {forkJoin} from "rxjs";
+import {timeSince} from "../../shared/helpers/timeSince";
 
 @Component({
   selector: 'app-game',
@@ -16,6 +19,7 @@ export class GameComponent implements OnInit {
   loading = true;
   loadingError = false;
   game!: GameWithDetails;
+  comments!: Comment[]
 
   constructor(private route: ActivatedRoute,
               private api: HttpClient,
@@ -27,15 +31,16 @@ export class GameComponent implements OnInit {
     this.route.paramMap.subscribe(params => {
       const gameKey = params.get('gameKey')!;
 
-      this.loadGameInfo(gameKey);
+      this.loadGame(gameKey);
     });
   }
 
-  private loadGameInfo(gameKey: string) {
-    this.api.get<GameWithDetails>(`games/${gameKey}`)
+  private loadGame(gameKey: string) {
+    forkJoin([this.getGame(gameKey), this.getComments(gameKey)])
       .subscribe({
-        next: game => {
+        next: ([game, comments]) => {
           this.game = game;
+          this.comments = comments
           this.loading = false;
         },
         error: err => {
@@ -52,8 +57,43 @@ export class GameComponent implements OnInit {
       });
   }
 
-  getGameImageUrl(game: Game): string
-  {
+  private getGame(gameKey: string) {
+    return this.api.get<GameWithDetails>(`games/${gameKey}`);
+  }
+
+  private getComments(gameKey: string) {
+    return this.api.get<Comment[]>(`comments?gameKey=${gameKey}`);
+  }
+
+  getGameImageUrl(game: Game): string {
     return `${env.apiUrl}games/${game.key}/image`;
+  }
+
+  get rootComments() {
+    return this.comments.filter(c => c.parentId === null);
+  }
+
+  getChildren(comment: Comment) {
+    return this.comments.filter(c => c.parentId === comment.id);
+  }
+
+  rootAndHasChildren(comment: Comment) {
+    return comment.parentId === null && this.getChildren(comment).length;
+  }
+
+  reply(comment: Comment) {
+
+  }
+
+  timeSince(comment: Comment){
+    return timeSince(comment.creationDate);
+  }
+
+  getFullUserNameFromComment(comment: Comment) {
+    return `${comment.userInfo.firstName} ${comment.userInfo.lastName}`;
+  }
+
+  getCommentUserAvatarUrl(comment: Comment) {
+    return `${env.apiUrl}avatar?username=${comment.userInfo.username}`;
   }
 }
