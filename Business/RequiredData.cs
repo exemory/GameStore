c577a2187a91acfb8930a58018d7a80447c8f7f6
Vlad <MadVlad1715@gmail.com>
2022-11-18 13:46:42 +0200

    Add seeding roles and creation admin user at app startup

---
+++ Business/RequiredData.cs
@@ -0,0 +1,18 @@
+﻿using Data.Entities;
+
+namespace Business;
+
+public static class RequiredData
+{
+    public static readonly IEnumerable<string> Roles = new[] {"Manager", "Admin"};
+
+    public static readonly User Admin = new()
+    {
+        UserName = "admin",
+        Email = "admin@gamestore.com",
+        FirstName = "Ivan",
+        LastName = "Ivanov"
+    };
+
+    public const string AdminPassword = "adminpass";
+}
--- Business/Services/DbInitializer.cs
+++ Business/Services/DbInitializer.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Interfaces;
 using Data;
+using Data.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Business.Services;
@@ -7,14 +9,47 @@ namespace Business.Services;
 public class DbInitializer : IDbInitializer
 {
     private readonly GameStoreContext _context;
+    private readonly RoleManager<IdentityRole<Guid>> _roleManager;
+    private readonly UserManager<User> _userManager;
 
-    public DbInitializer(GameStoreContext context)
+    public DbInitializer(GameStoreContext context, RoleManager<IdentityRole<Guid>> roleManager,
+        UserManager<User> userManager)
     {
         _context = context;
+        _roleManager = roleManager;
+        _userManager = userManager;
     }
 
     public async Task Initialize()
     {
         await _context.Database.MigrateAsync();
+        await SeedRequiredData();
+    }
+
+    private async Task SeedRequiredData()
+    {
+        await SeedRoles();
+        await AddAdmin();
+    }
+
+    private async Task SeedRoles()
+    {
+        if (!_roleManager.Roles.Any())
+        {
+            foreach (var roleName in RequiredData.Roles)
+            {
+                await _roleManager.CreateAsync(new IdentityRole<Guid>(roleName));
+            }
+        }
+    }
+
+    private async Task AddAdmin()
+    {
+        var adminExists = await _userManager.FindByNameAsync(RequiredData.Admin.UserName) != null;
+        if (!adminExists)
+        {
+            await _userManager.CreateAsync(RequiredData.Admin, RequiredData.AdminPassword);
+            await _userManager.AddToRoleAsync(RequiredData.Admin, "Admin");
+        }
     }
 }
