229ee71a809194485674ed02473a479ab111ad09
Vlad <MadVlad1715@gmail.com>
2022-11-24 18:13:56 +0200

    Add admin guard on '/users' route

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {GamesComponent} from "./components/games/games.component";
 import {GameComponent} from "./components/game/game.component";
 import {UsersComponent} from "./components/users/users.component";
+import {AdminGuard} from "./components/guards/admin.guard";
 
 const routes: Routes = [
   {path: 'games', component: GamesComponent},
   {path: 'games/:gameKey', component: GameComponent},
-  {path: 'users', component: UsersComponent},
+  {path: 'users', component: UsersComponent, canActivate: [AdminGuard]},
   {path: '**', redirectTo: 'games'}
 ];
 
